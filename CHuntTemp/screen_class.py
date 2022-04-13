e2618c6905824a0406c808d0a3a0f22344121387
Chunt0 <chunt.mail@gmail.com>
2022-04-13 11:25:13 -0700

    Updated 2022-04-13

--- CHuntTemp/screen_class.py
+++ CHuntTemp/screen_class.py
@@ -2,3 +2,16 @@
 # CS 162
 # Assignment 3
 # screen_class.py
+import turtle
+
+class Screen():
+    def __init__(self):
+        self.width = 600
+        self.height = 800
+    
+        screen = turtle.Screen()
+        screen.title("Snake Charmers")
+        screen.bgcolor("purple")
+        screen.screensize(width=self.width, height=self.height)
+
+window = Screen()
