81b4ddcab3ab8b005e6d5c635a03e857d92a8c41
Chunt0 <chunt.mail@gmail.com>
2022-04-13 13:42:22 -0700

    Updated 2022-04-13

--- CHuntTemp/screen_class.py
+++ CHuntTemp/screen_class.py
@@ -3,15 +3,15 @@
 # Assignment 3
 # screen_class.py
 import turtle
-import time
 
 w = 600
 h = 800
-    
-screen = turtle.Screen()
-screen.title("Snake Charmers")
-screen.bgcolor("purple")
-screen.screensize(width = w, height = h)
+
+while True:
+    screen=turtle.Screen()
+    screen.title("Snake Charmers")
+    screen.bgcolor("purple")
+    screen.screensize(width = w, height = h)
 
 
-screen.exitonclick()
+#    screen.exitonclick()
