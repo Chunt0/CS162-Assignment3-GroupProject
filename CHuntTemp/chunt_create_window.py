b8d5eaf7dab18c60aaebf98e44309f063265a77a
Chunt0 <chunt.mail@gmail.com>
2022-04-14 11:27:50 -0700

    Updated 2022-04-14

--- CHuntTemp/chunt_create_window.py
+++ CHuntTemp/chunt_create_window.py
@@ -1,13 +1,14 @@
 import turtle
 from chunt_food import Food
 from chunt_scoreboard import ScoreBoard
+from chunt_snake import Snake
 
 def createGameWindow():
     # move this into a screen class
     # Creating a window screen
     window = turtle.Screen()
     window.title("Snake Game")
-    window.bgcolor("light blue")
+    window.bgcolor("brown")
     # the width and height can be put as user's choice
     window.setup(width=800, height=800)
     return window
@@ -15,5 +16,6 @@ def createGameWindow():
 window = createGameWindow()
 score = ScoreBoard()
 food = Food()
+snake = Snake()
 window.exitonclick()
 
