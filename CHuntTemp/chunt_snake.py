70171fc3be6bad23d1c941bcb5e4713d748ba0ef
Chunt0 <chunt.mail@gmail.com>
2022-04-14 18:20:01 -0700

    Updated 2022-04-14

--- CHuntTemp/chunt_snake.py
+++ CHuntTemp/chunt_snake.py
@@ -1,13 +1,13 @@
 import turtle
-import utils
+import random
 
 class Snake:
     def __init__(self):
         """ This only runs once """
         self.head = turtle.Turtle()
         self.speed = 5
-        self.position = self.head.position()
-        self.head.shape('triangle')
+        self.body = []
+        self.head.shape("triangle")
         self.head.color("red")
         self.head.penup()
         self.head.direction = "Stop"
@@ -36,3 +36,30 @@ class Snake:
         segment.showturtle()
         self.body.append(segment)
         self.head.forward(self.speed)
+    
+    def addBody(self, scoreboard):
+        new_body = turtle.Turtle()
+        new_body.speed(0)
+        new_body.shape("square")
+        color = random.choice(["red", "orange", "yellow", "green", "blue", "purple",])
+        new_body.color(color)
+        new_body.penup()
+        self.body.append(new_body)
+        scoreboard.updateScore()
+
+    def slither(self):
+        if self.head.heading() == 0:  # Move 20 pixels right
+            x = self.head.xcor()
+            self.head.setx(x+20)
+        if self.head.heading() == 90:  # Move 20 pixels up
+            y = self.head.ycor()
+            self.head.sety(y+20)
+        if self.head.heading() == 180:  # Move 20 pixels left
+            x = self.head.xcor()
+            self.head.setx(x-20)
+        if self.head.heading() == 270:  # Move 20 pixels down
+            y = self.head.ycor()
+            self.head.setx(y-20)
+         
+
+
