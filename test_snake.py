cd9911cbf14be202a2f8a3e8ab9e5dd00a0e88ce
Phoenix Angulo <26429095+Kajmany@users.noreply.github.com>
2022-04-19 10:45:13 -0700

    Added testcase for many segments! Good performance still.

--- test_snake.py
+++ test_snake.py
@@ -1,34 +1,37 @@
 """File: test_snake.py
 Team No.: 1. THE SNAKE CHARMERS
 Author name: Phoenix Angulo, Sarah Mortensen, Christopher Hunt
-Date completed: 04/XX/2022
+Date completed: 04/19/2022
 Description: Sloppy testbed for snake movement and segments outside game context
 """
 
 import turtle, snake, time
 
-#Copy-pasting some setup from main in case it changes later
+# Copy-pasting some setup from main in case it changes later
 """Creates a new window"""
 window = turtle.Screen()
 window.title("Snake Charmer")
 window.bgcolor("light blue")
 window.setup(width=800, height=800)
-    
-window.tracer(0)#TODO: Evaluate utility
+
+window.tracer(0)  # TODO: Evaluate utility
 snake = snake.Snake()
 
 # Set up keyboard inputs.
 window.listen()
-window.onkey(snake.up, 'w')
-window.onkey(snake.down, 's')
-window.onkey(snake.left, 'a')
-window.onkey(snake.right, 'd')
+window.onkey(snake.up, "w")
+window.onkey(snake.down, "s")
+window.onkey(snake.left, "a")
+window.onkey(snake.right, "d")
+
+# for incr in range(0, 128):
+#    snake.add_body()
 
 while True:
     N = 0.1
     """Tick rate. For now?"""
 
     window.update()
-    # Increment snake head and body 
+    # Increment snake head and body
     snake.slither()
     time.sleep(N)
